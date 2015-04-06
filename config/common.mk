rohitchouhan07/vendor_benzo

enable gesture typing LatinIME

config/common.mk
@@ -42,6 +42,10 @@ PRODUCT_COPY_FILES += \
 PRODUCT_COPY_FILES += \
     vendor/eos/prebuilt/common/etc/init.eos.rc:root/init.eos.rc
 
+# Copy latinime for gesture typing
+PRODUCT_COPY_FILES += \
+    vendor/eos/prebuilt/common/lib/libjni_latinimegoogle.so:system/lib/libjni_latinimegoogle.so
+
 # Enable SIP+VoIP on all targets
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.software.sip.voip.xml:system/etc/permissions/android.software.sip.voip.xml

DesolationStaging/android_platform_vendor_deso

config/common.mk
@@ -53,6 +53,10 @@ PRODUCT_COPY_FILES += \
     vendor/deso/prebuilt/common/bin/sysrw:system/bin/sysrw  \
     vendor/deso/prebuilt/common/bin/sysrw:system/bin/sysro
 
+# Copy latinime for gesture typing
+PRODUCT_COPY_FILES += \
+    vendor/deso/prebuilt/common/lib/libjni_latinimegoogle.so:system/lib/libjni_latinimegoogle.so
+
 # Enable SIP and VoIP on all targets
 PRODUCT_COPY_FILES += \
 	frameworks/native/data/etc/android.software.sip.voip.xml:system/etc/permissions/android.software.sip.voip.xml